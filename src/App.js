5098ec1c78173cd7b3661dd1e9b324fe3f3c6a98
vin <didekqls93@gmail.com>
2023-02-20 17:12:44 +0900

    Route : RESTful Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./page/HomePage";
 import AboutPage from "./page/AboutPage";
-import { Routes, Route } from "react-router-dom";
+import ProductPage from "./page/ProductPage";
+import ProductDetailPage from "./page/ProductDetailPage";
 
 function App() {
   return (
@@ -9,9 +12,20 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="products" element={<ProductPage />} />
+        <Route path="products/:id" element={<ProductDetailPage />} />
       </Routes>
     </div>
   );
 }
+
+//RESTful Routes는 다음과 같다
+
+//HTTP Verb (HTTP명령어)를 사용하며
+
+//get : 데이터 가져오기
+//post : 데이터 생산
+//put : 데이터 수정
+//delete : 데이터 삭제
 
 export default App;
---
+++ src/page/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDetailPage = () => {
+  return (
+    <div>
+      <h1>Product Detail</h1>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
