f58d34a74096bad2a28e2778f304dfe1b960c337
vin <didekqls93@gmail.com>
2023-02-20 17:30:41 +0900

    Route : useSearchParams()

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const goProductPage = () => {
+    navigate("products?q=pants");
+  };
+  console.log();
   return (
     <div>
       <h1>Home Page</h1>
       <Link to="/about">Go to about page</Link>
+      <button onClick={goProductPage}>Pants</button>
     </div>
   );
 };
--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const ProductPage = () => {
+  let [query, setQuery] = useSearchParams();
+  console.log("query??", query.get("q"));
   return (
     <div>
       <h1>Product Page</h1>
