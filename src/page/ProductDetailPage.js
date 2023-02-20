b91fb84a7a4482decc6e41b627a5d1015ebf42fd
vin <didekqls93@gmail.com>
2023-02-20 17:18:33 +0900

    Route: useParams

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
+  const { id } = useParams();
   return (
     <div>
-      <h1>Product Detail</h1>
+      <h1>Product Detail{id}</h1>
     </div>
   );
 };
